1119eac53e4951d1b72adcc7ff73c7869f93dd39
Iqrash Jalil <iqrashjalil@gmail.com>
2024-11-08 22:19:31 +0500

    Adjust some ugs

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -2,9 +2,12 @@ const sendToken = (user, statusCode, res) => {
   const token = user.generateToken();
 
   const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
     httpOnly: true, // Makes the cookie inaccessible to JavaScript on the client side
     secure: true, // Ensures the cookie is sent only over HTTPS
-    sameSite: "Lax", // Helps prevent CSRF attacks
+    sameSite: "Strict", // Helps prevent CSRF attacks
     maxAge: 24 * 60 * 60 * 1000,
   };
 
