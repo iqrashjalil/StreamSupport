e0ca9f2e6c4f978108ddc4568e5cc2de60513f94
Iqrash Jalil <iqrashjalil@gmail.com>
2024-11-08 22:34:16 +0500

    Adjust Some Bugs

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,7 +7,7 @@ const sendToken = (user, statusCode, res) => {
     ),
     httpOnly: true, // Makes the cookie inaccessible to JavaScript on the client side
     secure: true, // Ensures the cookie is sent only over HTTPS
-    sameSite: "Strict", // Helps prevent CSRF attacks
+    sameSite: "none", // Helps prevent CSRF attacks
     maxAge: 24 * 60 * 60 * 1000,
   };
 
