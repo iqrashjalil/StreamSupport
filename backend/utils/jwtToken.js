3ac963ed845b7ed0dc395b00547890fad0c0772b
Iqrash Jalil <iqrashjalil@gmail.com>
2024-11-08 21:36:32 +0500

    Adjust a alittle bit of cookies settings

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -2,12 +2,10 @@ const sendToken = (user, statusCode, res) => {
   const token = user.generateToken();
 
   const options = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Lax",
+    httpOnly: true, // Makes the cookie inaccessible to JavaScript on the client side
+    secure: process.env.NODE_ENV === "production", // Ensures the cookie is sent only over HTTPS
+    sameSite: "Lax", // Helps prevent CSRF attacks
+    maxAge: 24 * 60 * 60 * 1000,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
